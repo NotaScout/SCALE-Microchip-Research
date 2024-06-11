5f08e2d21bb5e71cc740348d09b6f82fd1658121
NotaScout <154877329+NotaScout@users.noreply.github.com>
2024-06-11 15:20:49 -0500

    Update OpenCV-TEST1.py

--- OpenCV-TEST1.py
+++ OpenCV-TEST1.py
@@ -9,6 +9,8 @@ image stretching to get better results
 
 text display on image
 
+save recognized text to file
+
 
 '''
 
@@ -85,7 +87,9 @@ image_path = ROOT_PATH + "\\" + input_image
 print(image_path)
 
 # sends image data to image variable
-image = cv2.imread(image_path)
+original_image = cv2.imread(image_path)
+image = original_image.copy()
+
 # converts the image to greyscale
 grey_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # shows us a greyscale version of our orignal image
@@ -126,7 +130,8 @@ exact_contours, hierarchy = cv2.findContours(thresholded_image, cv2.RETR_TREE, c
  
  # third input arg is id, basically it specifies which contours to draw. -1 draws all
 print(pytesseract.image_to_string(image))
-exact_image_contour = cv2.drawContours(image,exact_contours,-1,127) 
+exact_image_contour = cv2.drawContours(image,exact_contours,-1,(31,255,31)) 
+
 
 #cv2.imshow("Exact Contour", exact_image_contour)
 
@@ -144,11 +149,17 @@ recognized_text_file.close()
 cv2.destroyAllWindows()
 
 # replace with increment in future
-contour_count = exact_contours[1] # just takes the first contour
+# also another note: if we just do the 0th one, it appears to just capture the entire image. ie. (0,0,[image size])
+contour_count = exact_contours[2] # just takes the first contour
 
 # then we need to extract the x,y coords as well as the width and height of the bounding box on our contour
 x,y,w,h = cv2.boundingRect(contour_count)
 print(cv2.boundingRect(contour_count))
+
+bounding_box_test = cv2.rectangle(original_image,(x,y),(x+w,y+h),(31,255,31),3)
+cv2.imshow('Bounding Box',bounding_box_test)
+cv2.imshow('Original',original_image)
+cv2.waitKey(0)
 
 
 # what we need:
