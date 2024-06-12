9ac773cc55be416dc29086de757a1ee4c4c14ff7
NotaScout <154877329+NotaScout@users.noreply.github.com>
2024-06-12 15:03:21 -0500

    Update OpenCV-TEST1.py

--- OpenCV-TEST1.py
+++ OpenCV-TEST1.py
@@ -1,4 +1,4 @@
-'''
+"""
 TODO:
 
 Make tesseract also install-able for all users (in addition to keeping the one user thing)
@@ -12,12 +12,11 @@ text display on image
 save recognized text to file
 
 finish rect dilation
-'''
-
+"""
 
 import cv2
 
-import os 
+import os
 
 import time
 
@@ -27,50 +26,53 @@ import pytesseract
 
 ## stuff we need
 
-#pip install opencv-python
-#pip install pytesseract
+# pip install opencv-python
+# pip install pytesseract
 
 # to print a list of all environment varables:
-#print(os.environ)
+# print(os.environ)
 
 
-'''
+"""
 TEST BOX
-'''
+"""
 
-#rectangle_kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(kernelsize,kernelsize))
-#rectangle_kernel
+# rectangle_kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(kernelsize,kernelsize))
+# rectangle_kernel
 
-# X Centroid: 
+# X Centroid:
 # cx = int(M['m10']/M['m00'])
 # Y Centroid:
 # cy = int(M['m01']/M['m00'])
 
 
-
-'''
+"""
 </>
 
-'''
+"""
 
 
-'''
+"""
 Defines
-'''
+"""
 
 # larger sizes detect more grouped words, ie sentences
-kernelsize = 5
+kernelsize = 18
 
-#tesseract path
+# tesseract path
 #'C:\Users\scho3988\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
-#so
-#os.environ["HOMEPATH"] + "\\AppData\\Local\\Programs\\Tesseract-OCR\\tesseract.exe"
+# so
+# os.environ["HOMEPATH"] + "\\AppData\\Local\\Programs\\Tesseract-OCR\\tesseract.exe"
 
 # install for 1 user
-pytesseract.pytesseract.tesseract_cmd = os.environ["HOMEDRIVE"]+os.environ["HOMEPATH"] + "\\AppData\\Local\\Programs\\Tesseract-OCR\\tesseract.exe"
+pytesseract.pytesseract.tesseract_cmd = (
+    os.environ["HOMEDRIVE"]
+    + os.environ["HOMEPATH"]
+    + "\\AppData\\Local\\Programs\\Tesseract-OCR\\tesseract.exe"
+)
 
 
-rectangle_kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(kernelsize,kernelsize))
+rectangle_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (kernelsize, kernelsize))
 
 # we need to apply a dilation to our image to "smooth" it out. Essentially we need the text to all be connected physically together for it
 # to register as 1 block of text.
@@ -99,7 +101,7 @@ original_image = cv2.imread(image_path)
 # all cv image functions are destructive as far as I'm aware #
 image = original_image.copy()
 
-h,w = image.shape[:2]
+h, w = image.shape[:2]
 
 print("Height = {}, Width = {}".format(h, w))
 
@@ -107,63 +109,68 @@ print("Height = {}, Width = {}".format(h, w))
 # converts the image to greyscale
 grey_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # shows us a greyscale version of our orignal image
-#cv2.imshow('ImageWindow', grey_image)
-'''
+cv2.imshow("ImageWindow", grey_image)
+"""
 
 
-'''
-
-
-# now we dilate the greyscale image
-
-dilation_image = cv2.dilate(grey_image,rectangle_kernel)
-
-cv2.imshow("{} by {} Dilation".format(kernelsize,kernelsize),dilation_image)
+"""
 
 
 # waits for keypress input to continue
 cv2.waitKey(0)
 
-# return image, 
+# return image,
 
-'''
+"""
 THRESHOLDING
 ret, thresholded_image = cv.threshold(image, lower_threshold, upper_threshold,threshold_algorithm)
 I dont know what ret means exactly (return?)
 
-'''
+"""
 
 # thresholding the image
-ret, thresholded_image = cv2.threshold(grey_image, lower_threshold, upper_threshold, cv2.THRESH_BINARY)
+ret, thresholded_image = cv2.threshold(
+    grey_image, lower_threshold, upper_threshold, cv2.THRESH_BINARY_INV
+)
 
-#cv2.imshow('Binary Threshold Applied', thresholded_image)
+
+# now we dilate the greyscale image
+
+dilation_image = cv2.dilate(thresholded_image, rectangle_kernel, iterations=2)
+
+cv2.imshow("{} by {} Dilation".format(kernelsize, kernelsize), dilation_image)
+
+
+# cv2.imshow('Binary Threshold Applied', thresholded_image)
 cv2.waitKey(0)
 # Locate Contours
 # input. contours, hierarchy = cv.findContours(thresholded_img, contour retrival mode, contour approximation mode)
-exact_contours, hierarchy = cv2.findContours(thresholded_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-# is hierarcy 
+exact_contours, hierarchy = cv2.findContours(
+    dilation_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE
+)
+# is hierarcy
 
-#cv2.imshow('Output',contours)
-#print("Contours = {}", format(contours))
-#contour_data = numpy.array(contours)
+# cv2.imshow('Output',contours)
+# print("Contours = {}", format(contours))
+# contour_data = numpy.array(contours)
 
-#print("Raw Image Data = {}", format(contour_data))
- 
- # third input arg is id, basically it specifies which contours to draw. -1 draws all
+# print("Raw Image Data = {}", format(contour_data))
+
+# third input arg is id, basically it specifies which contours to draw. -1 draws all
 print(pytesseract.image_to_string(image))
-exact_image_contour = cv2.drawContours(image,exact_contours,-1,(31,255,31)) 
+exact_image_contour = cv2.drawContours(image, exact_contours, -1, (31, 255, 31))
 
 
-#cv2.imshow("Exact Contour", exact_image_contour)
+# cv2.imshow("Exact Contour", exact_image_contour)
 
 cv2.waitKey(0)
 
-'''
+"""
 OCR
-'''
+"""
 
-recognized_text_file = open("recognized_text.txt","w+")
-recognized_text_file.write("") # clear file
+recognized_text_file = open("recognized_text.txt", "w+")
+recognized_text_file.write("")  # clear file
 recognized_text_file.close()
 # <> net recognized text (control group)
 
@@ -171,18 +178,30 @@ cv2.destroyAllWindows()
 
 # replace with increment in future
 # also another note: if we just do the 0th one, it appears to just capture the entire image. ie. (0,0,[image size])
-contour_count = exact_contours[1] # just takes the first contour
+for count in 3:  # {
+    contour_count = exact_contours[count]  # just takes the first contour
 
 # then we need to extract the x,y coords as well as the width and height of the bounding box on our contour
-x,y,w,h = cv2.boundingRect(contour_count)
+x, y, w, h = cv2.boundingRect(contour_count)
 print(cv2.boundingRect(contour_count))
 
-bounding_box_test = cv2.rectangle(original_image,(x,y),(x+w,y+h),(31,255,31),3)
-cv2.imshow('Bounding Box',bounding_box_test)
-cv2.imshow('Original',original_image)
+bounding_box_test = cv2.rectangle(
+    original_image, (x, y), (x + w, y + h), (31, 255, 31), 3
+)
+cv2.imshow("Bounding Box", bounding_box_test)
+# cv2.imshow('Original',original_image)
 cv2.waitKey(0)
+# }
 
 
+cropped_image = original_image[y : y + h, x : x + w]
+# cv2.imshow('Cropped',cropped_image)
+
+
+# now we can feed each individual cropped image into OCR to be processed
+
+
+# cv2.waitKey(0)
 # what we need:
 
 # contours from the source image
@@ -190,8 +209,7 @@ cv2.waitKey(0)
 # apply OCR
 
 
-
-'''
+"""
 How to Contour trace (edge detect):
 Step 1
 read image <<< done
@@ -204,10 +222,10 @@ Step 4
 findcontours () funciton << done
 Step 5
 Draw Contours on original input IMG <<< done
-'''
+"""
 
 
-'''
+"""
 To apply OCR (text detect)
 Step 1
 get nth contour 
@@ -215,10 +233,7 @@ Step 2
 draw rect
 Step 3
 
-'''
-
+"""
 
 
 ## delete // below this line //
-
-
