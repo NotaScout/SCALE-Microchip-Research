c9d37438f52abd6aa0745cfb871e6c82432b514a
NotaScout <154877329+NotaScout@users.noreply.github.com>
2024-06-25 22:19:45 -0500

    ++ more lib stuff

--- lib/im_proc.py
+++ lib/im_proc.py
@@ -3,6 +3,22 @@ import cv2
 
 # we can have each individual part of the processing separate, then bring them together in 1 function.
 
+ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+
+
+FILENAME = "Detected Items.txt"
+
+# helper sub-routines
+
+def find_centroid(x_start,y_start,width,height):
+    x_centroid = x_start+width/2
+    y_centroid = y_start+height/2
+    return x_centroid,y_centroid
+
+def crop_image(input_image,x_start,y_start,width,height):
+    cropped_image = input_image[y_start:y_start+height, x_start:x_start+width]
+    return cropped_image
+
 
 
 def threshold_image(input_image,upper_threshold = 255, lower_threshold = 127):
@@ -17,7 +33,6 @@ def threshold_image(input_image,upper_threshold = 255, lower_threshold = 127):
     return thresholded_image
 
 
-
 def find_contours(thresholded_image,kernelsize = 15):
     
 
@@ -28,7 +43,12 @@ def find_contours(thresholded_image,kernelsize = 15):
     contours, hierarchy = cv2.findContours(
     dilation_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE
     )
-    return contours
+    return contours, hierarchy
+
+def process_image(input_image,): # input image, and processing type
+
+
+
 
 # def read_contours
 
@@ -37,6 +57,21 @@ def find_contours(thresholded_image,kernelsize = 15):
 
 
 
+def write_log(FILEPATH,saved_text):
+    # write out the objects found to a text file and save it
+    if FILEPATH == 0:
+        FILEPATH = 
+
+    
+    file_write = open("FILEPATH","w+")
+    file_write.write("") # clear file
+    file_write.close()
+
+
+def apply_model(input_image):
+    threshold_img = threshold_image(input_image)
+
+    
     
     
 
